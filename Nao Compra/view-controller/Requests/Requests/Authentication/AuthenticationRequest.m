c9b21df1aa2d07c2b9dc085436e48b40819f702b
Jorge Henrique de Sousa Rocha <jhrocha.sousa@gmail.com>
2016-06-26 01:54:37 -0300

    Signup created

---
+++ Nao Compra/view-controller/Requests/Requests/Authentication/AuthenticationRequest.m
@@ -0,0 +1,54 @@
+//
+//  AuthenticationRequest.m
+//  Nao Compra
+//
+//  Created by Jorge Henrique Rocha on 26/06/16.
+//  Copyright © 2016 Jorge Henrique Rocha. All rights reserved.
+//
+
+#import "AuthenticationRequest.h"
+@implementation AuthenticationRequest
+
+- (void)CreateUser:(Signup *)signup withBlock:(blockRequestSignup)block{
+
+    NSString *url= [NSString stringWithFormat:@"%@%@",API_INITIAL_PRODUCTION_PATH, SIGNUP];
+    NSError *json_error;
+    NSDictionary * params= [MTLJSONAdapter JSONDictionaryFromModel:signup error:&json_error];
+    
+    if (json_error) {
+        if (block) { block(NO,nil,json_error); }
+    }else{
+
+        [self POSTRequestWithURL:url parameters:params withBlock:^(BOOL success, id result, NSError *req_error) {
+            
+            if (!req_error) {
+                
+                NSError *model_error;
+                SignupResponse *response= [MTLJSONAdapter modelOfClass:SignupResponse.class fromJSONDictionary:result error:&model_error];
+                if (response) {
+                    if (block) {
+                        block(YES,response,nil);
+                    }
+                    
+                }else{
+                    if (block) {
+                        block(NO,nil,model_error);
+                    }
+                }
+                
+            }else{
+                if (block) {
+                    block(NO,nil,req_error);
+                }
+            }
+            
+        }];
+    }
+    
+
+}
+- (void)SigninUser:(Signin *)signin withBlock:(blockRequestSignin)block{
+    
+}
+
+@end
