7ebd4ac4651736ba2f3f241f0bbbef39f13b197f
Jorge Henrique de Sousa Rocha <jhrocha.sousa@gmail.com>
2016-06-26 12:59:03 -0300

    Login view added on storyboard

---
+++ Nao Compra/controller/Authentication/AuthenticationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AuthenticationViewController.h
+//  Nao Compra
+//
+//  Created by Jorge Henrique Rocha on 26/06/16.
+//  Copyright © 2016 Jorge Henrique Rocha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AuthenticationViewController : UIViewController
+
+@end
