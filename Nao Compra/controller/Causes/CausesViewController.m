0274f7967901e39098126de0ef1b61e35a3de3dd
Jorge Henrique de Sousa Rocha <jhrocha.sousa@gmail.com>
2016-06-26 23:04:30 -0300

    View controller integrated on storyboard

---
+++ Nao Compra/controller/Causes/CausesViewController.m
@@ -0,0 +1,45 @@
+//
+//  CausesTableViewController.m
+//  Nao Compra
+//
+//  Created by Jorge Henrique Rocha on 26/06/16.
+//  Copyright © 2016 Jorge Henrique Rocha. All rights reserved.
+//
+
+#import "CausesViewController.h"
+
+@implementation CausesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.cause= [Cause new];
+    self.request= [CausesRequests new];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (IBAction)chooseGender:(id)sender {
+
+}
+
+- (IBAction)chooseAgegroup:(id)sender {
+
+}
+
+- (IBAction)chooseQuestion:(id)sender {
+
+}
+
+- (IBAction)chooseAnswer:(id)sender {
+
+}
+
+- (IBAction)sendData:(id)sender {
+
+}
+@end
